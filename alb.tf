e0c3b8b3d32752cd13391172f954215c839d79c1
Behdad Tabrizi <b.tabrizi@deroso.de>
2026-04-28 12:01:54 +0200

    [fix] Resolve file upload Axios error in target groups

--- alb.tf
+++ alb.tf
@@ -20,6 +20,11 @@ resource "aws_lb_target_group" "app" {
     unhealthy_threshold = 2
     interval            = 30
   }
+
+  stickiness {
+    type            = "lb_cookie"
+    cookie_duration = 86400
+  }
 }
 
 resource "aws_lb_listener" "http" {
